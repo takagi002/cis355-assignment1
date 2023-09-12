7ccf38c26b61243e592143a401129f32853d51b3
Julia Takagi <julia@JuliasMacStudio-735.lan>
2023-09-11 20:24:56 -0400

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,10 @@
+require 'greeter'
+
 # Default is "World"
 # Author: Julia Takagi (jtakagi@proton.me)
 name = ARGV.first || "World"
 
 puts "Hello, #{ARGV.first}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
